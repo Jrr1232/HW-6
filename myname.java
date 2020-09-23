581a85ef8bef4fc7e28bda136312d5f452804371
Junior <jforencio@njcu.edu>
2020-09-23 02:24:45 +0000

    second commit

--- myname.java
+++ myname.java
@@ -1,6 +1,6 @@
 public class myname{
 	public static void main(String[] args){
-		System.out.println("My name is Junior");
+		System.out.println("My favorite programming language is Java");
 	}
 }
 
